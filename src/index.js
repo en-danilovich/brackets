1c289d9b5b5000d94cea6e1cb6993a3180424bc3
Eugene Danilovich <bluerayend@gmail.com>
2023-02-07 19:06:04 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = [].concat(...bracketsConfig);
+
+  let stack = [];
+  for (let char of str) {
+    let bracketIndex = bracketsConfig.indexOf(char);
+    if (bracketIndex === -1) {
+      continue;
+    }
+
+    if (bracketsConfig[bracketIndex] === bracketsConfig[bracketIndex + 1] && stack.slice(-1)[0] === bracketIndex + 1) {
+      bracketIndex++;
+    }
+    if (bracketIndex % 2 === 0) {
+      stack.push(bracketIndex + 1);
+    } else if (stack.pop() !== bracketIndex) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 }
